56cfb9f5c25b4715856c407f9f5e2c9d51022db0
lucas <razafymahandryta@gmail.com>
2026-05-13 08:47:26 +0300

    feat(auth): implement AuthRepository for database isolation

---
+++ backend/src/features/auth/auth.repository.ts
@@ -0,0 +1,46 @@
+import { prisma } from "@/config/prisma.js";
+import {
+  userSafeSelect,
+  UserSafe,
+  RegisterInput,
+  UserComplete,
+  userCompleteSelect,
+} from "./auth.types.js";
+
+export class authRepository {
+  // register function
+  async createUser(data: RegisterInput): Promise<UserSafe> {
+    return await prisma.user.create({
+      data: {
+        id: data.id,
+        firstname: data.firstname,
+        lastname: data.lastname,
+        email: data.email,
+        role: data.role,
+        password: data.role,
+      },
+      select: userSafeSelect,
+    });
+  }
+  // find by email
+  async findByEmail(email: string): Promise<UserComplete | null> {
+    return await prisma.user.findUnique({
+      where: { email },
+      select: userCompleteSelect,
+    });
+  }
+  // verify if an email already exist
+  async exists(email: string): Promise<boolean> {
+    const count = await prisma.user.count({
+      where: { email },
+    });
+    return count > 0;
+  }
+  // find by id
+  async findById(id: string): Promise<UserSafe | null> {
+    return await prisma.user.findUnique({
+      where: { id },
+      select: userSafeSelect,
+    });
+  }
+}
