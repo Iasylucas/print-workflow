aaa3c7a288a3b85ea77430f1a4ff3a38bb9a5c6e
lucas <razafymahandryta@gmail.com>
2026-05-14 15:53:29 +0300

    feat(infra): complete server architecture, routing and admin seeding

--- backend/src/middlewares/error.middleware.ts
+++ backend/src/middlewares/error.middleware.ts
@@ -3,12 +3,7 @@ import { AppError } from "@/shared/error/error.js";
 import { ZodError } from "zod";
 import { env } from "@/config/env.js";
 
-export const errorMiddleware = (
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+export const errorMiddleware = (err: Error, req: Request, res: Response) => {
   // error for zod validation
   if (err instanceof ZodError) {
     return res.status(400).json({
